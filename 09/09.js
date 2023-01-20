eeed5b2fbbf32a2573de445ee583ecd3c23842f3
naeunk1m <107620483+naeunk1m@users.noreply.github.com>
2023-01-20 14:13:11 +0900

    2023-01-20

--- 09/09.js
+++ 09/09.js
@@ -11,3 +11,62 @@
 
 // console.log(typeof str, str);
 // console.log(typeof x, x);
+
+// ex 02
+// var x = 10;
+
+// 암묵적 타입 변환
+// var str = x + "";
+// console.log(typeof str, str);
+
+// console.log(typeof x, x);
+
+// ex 11
+// if ("") console.log("1");
+// if (true) console.log("2");
+// if (0) console.log("3");
+// if ("str") console.log("4");
+// if (null) console.log("5");
+
+// ex 26
+
+// var elem = null;
+
+// var value = elem?.value;
+// console.log(value);
+
+// ex 27
+
+// var elem = null;
+
+// var value = elem && elem.value;
+// console.log(value);
+
+// ex 28
+
+// var str = "";
+
+// var length = str && str.length;
+
+// console.log(length);
+
+// ex 29
+
+// var str = "";
+
+// var length = str?.length;
+// console.log(length);
+
+// ex 30
+// var foo = null ?? "default string";
+// console.log(foo);
+
+// ex 31
+
+// var foo = "" || "default string";
+// console.log(foo);
+
+// ex 32
+
+// var foo = "" ?? "default string";
+// console.log(foo);
